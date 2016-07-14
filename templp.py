c3794f1d5844eaa309f8572cd4c5752317946b99
ulx <hersh247@gmail.com>
2016-07-14 14:41:16 -0700

    commit

--- templp.py
+++ templp.py
@@ -16,12 +16,16 @@ class Import_conf():
 	def generate_ic(self):
 		#--import config_data file
 		config_data = pd.read_csv("{0}".format(self.i_data), index_col='host')
-		#--get the list of all hosts
+		#--get the list of all hosts#
+		# create ouput dir
+		mkdir_o = subprocess.Popen("mkdir {0}_output".format(self.i_base), shell=True)
+		# wilt untill dir is created
+		mkdir_ow = mkdir_o.communicate()[0]
 		xhostconfig_hosts = config_data.index
 		for host in xhostconfig_hosts:
 			print "generating config for {0}".format(host)
 			#--geenerate a copy of a the base file
-			shutil.copy2("{0}".format(self.i_base), "config_output/{0}".format(host))
+			shutil.copy2("{0}".format(self.i_base), "{0}_output/{1}".format(self.i_base,host))
 			#--get the row for the host, ie the host to genereate conig for
 			xhostconfig = config_data.loc["{0}".format(host)]
 			#--get the columns of the host, ie the items to replace
@@ -34,7 +38,7 @@ class Import_conf():
 				replace_item_with = xhostconfig["{0}".format(config_item)]
 				#--find the config item to replace
 				#--replace the config item
-				hostconfig_file = fileinput.input(files=("config_output/{0}".format(host)),inplace=1)
+				hostconfig_file = fileinput.input(files=("{0}_output/{1}".format(self.i_base,host)),inplace=1)
 				for line in hostconfig_file:
 					replace = line.replace("<{0}>".format(replace_item), "{0}".format(replace_item_with))
 					print replace,
@@ -44,7 +48,7 @@ class Import_conf():
 			print "======================"
 
 	def file_list(self):
-		filelist = subprocess.check_output(['ls -l config_output | awk -F\' \' \'{ print $9 }\' | sort -n '], shell=True)
+		filelist = subprocess.check_output(['ls -l {0}_output | awk -F\' \' \'{{ print $9 }}\' | sort -n '.format(self.i_base)], shell=True)
 		filelist = filelist.split()
 		return filelist
 	
@@ -55,7 +59,7 @@ class Import_conf():
 		with open("{0}".format(o_file), "a") as base_file:
 			for file in Import_conf.file_list(self):
 				print ("starting {0}".format(file))
-				with open("config_output/{0}".format(file), "r") as source_file:
+				with open("{0}_output/{1}".format(self.i_base,file), "r") as source_file:
 						for line in source_file:
 							base_file.write(line)
 							#print line,
